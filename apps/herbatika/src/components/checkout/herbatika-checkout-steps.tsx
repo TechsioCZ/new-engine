7ca2e43314a3ae075fa2c3631d597cc1fa78a1c3
KaiUweCZE <pisezdenekpise@seznam.cz>
2026-04-08 17:48:13 +0200

    refactor checkout steps

--- apps/herbatika/src/components/checkout/herbatika-checkout-steps.tsx
+++ apps/herbatika/src/components/checkout/herbatika-checkout-steps.tsx
@@ -1,4 +1,5 @@
 import { Icon } from "@techsio/ui-kit/atoms/icon";
+import { Tooltip } from "@techsio/ui-kit/atoms/tooltip";
 import { Steps } from "@techsio/ui-kit/molecules/steps";
 
 export type HerbatikaCheckoutStepItem = {
@@ -17,20 +18,23 @@ export function HerbatikaCheckoutSteps({
 }: HerbatikaCheckoutStepsProps) {
   return (
     <Steps count={steps.length} linear size="sm" step={step}>
-      <Steps.List className="w-fit mx-auto justify-start p-300 rounded-xl">
+      <Steps.List className="w-full md:w-fit mx-auto md:justify-start p-300 rounded-xl">
         {steps.map((item, index) => (
-          <Steps.Item index={index} key={item.id} className="flex-none min-w-max">
-            <Steps.Trigger disabled>
+          <Steps.Item index={index} key={item.id} className="flex-none min-w-max gap-x-450 sm:gap-x-150">
+            <Steps.Trigger className="min-h-750 min-w-750 justify-center" disabled>
               <Steps.Indicator>
+                <Tooltip className="inline-flex sm:hidden" content={item.title} placement="bottom">
                 <Steps.Status
                 complete={<Steps.Number />}
                 current={<Steps.Number />}
                 incomplete={<Steps.Number />}
                 />
+                </Tooltip>
               </Steps.Indicator>
-              <Steps.ItemText>
+              <Steps.ItemText className="hidden sm:inline-flex">
                 <Steps.Title>{item.title}</Steps.Title>
               </Steps.ItemText>
+              
             </Steps.Trigger>
             {steps.length -1 > index && <Icon icon="token-icon-chevron-right" size="xl" />}
           </Steps.Item>
--- apps/herbatika/src/components/checkout/sections/checkout-steps-section.tsx
+++ apps/herbatika/src/components/checkout/sections/checkout-steps-section.tsx
@@ -14,7 +14,7 @@ export function CheckoutStepsSection({
 }: CheckoutStepsSectionProps) {
   return (
     <section className="flex w-full justify-center max-w-max-w mx-auto">
-      <div className="flex min-h-850 w-full max-w-auth-content items-center justify-center rounded-xl bg-surface px-400">
+      <div className="flex min-h-850 w-full max-w-auth-content items-center justify-center rounded-xl bg-surface px-300 sm:px-400">
         <div className="w-full overflow-x-auto">
           <HerbatikaCheckoutSteps step={checkoutStepIndex} steps={steps} />
         </div>
