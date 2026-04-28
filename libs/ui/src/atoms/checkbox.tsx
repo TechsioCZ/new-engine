dbd8d4b11bc3a0076e7a9ce2e902de3c65e4cdb5
KaiUweCZE <pisezdenekpise@seznam.cz>
2026-04-28 18:14:17 +0200

    fix(ui): remove duplicate checkbox invalid ring

--- libs/ui/src/atoms/checkbox.tsx
+++ libs/ui/src/atoms/checkbox.tsx
@@ -30,9 +30,6 @@ const checkboxVariants = tv({
     "focus-visible:outline-offset-(length:--default-ring-offset)",
     "data-invalid:border-(length:--border-width-validation)",
     "data-invalid:border-checkbox-border-error",
-    "data-invalid:outline-(style:--default-ring-style) data-invalid:outline-(length:--default-ring-width)",
-    "data-invalid:outline-checkbox-ring-error",
-    "data-invalid:outline-offset-(length:--default-ring-offset)",
   ],
 })
 
