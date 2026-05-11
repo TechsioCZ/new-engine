adf021ac7914c437cf9315e9ba2b6771113818b9
Kai Uwe <pisezdenekpise@seznam.cz>
2026-05-11 17:21:23 +0200

    fix ui blog author card

--- apps/herbatika/src/components/blog/blog-author-card.tsx
+++ apps/herbatika/src/components/blog/blog-author-card.tsx
@@ -7,24 +7,24 @@ type BlogAuthorCardProps = {
 
 export function BlogAuthorCard({ post }: BlogAuthorCardProps) {
   return (
-    <section className="flex flex-col gap-300 rounded-2xl border border-border-secondary bg-surface p-400 sm:flex-row sm:items-center">
+    <section className="flex flex-col gap-500 rounded-2xl border border-border-secondary bg-surface p-400 sm:flex-row sm:items-center">
       <NextImage
         alt={post.author}
-        className="h-850 w-850 rounded-md object-cover"
-        height={64}
+        className="h-blog-detail-author-image aspect-square rounded-md object-cover"
+        height={124}
         src={post.authorImageSrc}
-        width={64}
+        width={124}
         quality={50}
       />
 
-      <div className="space-y-150">
+      <div className="space-y-200">
         <p className="text-xs leading-normal text-fg-secondary">
           {post.authorRole}
         </p>
         <h3 className="text-xl leading-tight font-bold text-fg-primary">
           {post.author}
         </h3>
-        <p className="text-sm leading-relaxed text-fg-secondary">
+        <p className="text-md leading-relaxed text-fg-secondary">
           {post.authorBio}
         </p>
       </div>
