0fccdfd96cc86ce26acd41e2912ad21bc036ee5c
Kai Uwe <pisezdenekpise@seznam.cz>
2026-05-18 09:51:32 +0200

    fix redirect in account popover

--- apps/herbatika/src/components/auth/auth-footer.tsx
+++ apps/herbatika/src/components/auth/auth-footer.tsx
@@ -1,25 +1,24 @@
-import NextLink from "next/link"
+import NextLink from "next/link";
 
 type AuthFooterProps = {
-    text: string,
-    href: string,
-    linkText: string
-}
+  text: string;
+  href: string;
+  linkText: string;
+};
 
-export const AuthFooter = ({text, href, linkText}: AuthFooterProps) => {
-
-    return(
-        <div className="mt-400 border-t border-border-secondary pt-300 text-center">
-          <p className="text-sm text-fg-secondary">
-            {`${text} `}
-            <NextLink
-              className="font-medium text-primary underline-offset-4 hover:underline"
-              href={href}
-            >
-              {linkText}
-            </NextLink>
-          </p>
-      </div>
-
-    )
-}
+export const AuthFooter = ({ text, href, linkText }: AuthFooterProps) => {
+  return (
+    <div className="mt-400 border-t border-border-secondary pt-300 text-center">
+      <p className="text-sm text-fg-secondary">
+        {`${text} `}
+        <NextLink
+          className="font-medium text-primary underline-offset-4 hover:underline"
+          href={href}
+          onMouseDown={(event) => event.preventDefault()}
+        >
+          {linkText}
+        </NextLink>
+      </p>
+    </div>
+  );
+};
