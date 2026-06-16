bd8e173503dd35c9d574041ec8829f4caa84e94d
Lukas Chylik <chylik.lukas@gmail.com>
2026-06-16 07:46:22 +0200

    Revert "chore(storybook): remove empty addon-links and addon-onboarding stubs"
    This reverts commit d8fc08aad3fbd0ef9bde1bcb5fc54794eddd8f2a.

--- libs/ui/.storybook/main.ts
+++ libs/ui/.storybook/main.ts
@@ -6,6 +6,8 @@ const config: StorybookConfig = {
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
     '@storybook/addon-themes',
     '@techsio/storybook-better-a11y',
     'storybook-addon-rslib',
