93e8aa74ac216f332b6fe42eddb9721b1995b82b
Petr Glaser <syreanis+1@gmail.com>
2026-06-25 23:35:52 +0200

    chore: align smart suggest libs with rslib

---
+++ libs/smart-suggest/indexing/rslib.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "@rslib/core"
+
+export default defineConfig({
+  source: {
+    entry: {
+      index: "./src/**/*.{ts,tsx}",
+    },
+  },
+  lib: [
+    {
+      bundle: false,
+      dts: true,
+      format: "esm",
+    },
+  ],
+})
---
+++ libs/smart-suggest/ui/rslib.config.ts
@@ -0,0 +1,21 @@
+import { pluginReact } from "@rsbuild/plugin-react"
+import { defineConfig } from "@rslib/core"
+
+export default defineConfig({
+  source: {
+    entry: {
+      index: "./src/**/*.{ts,tsx}",
+    },
+  },
+  lib: [
+    {
+      bundle: false,
+      dts: true,
+      format: "esm",
+    },
+  ],
+  output: {
+    target: "web",
+  },
+  plugins: [pluginReact()],
+})
